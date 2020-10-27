a9b14def6f27a784a1ebf827eb441748ed8ceec4
Minh-Phuong Tran <minh-phuong.tran@student.uclouvain.be>
2020-10-27 18:39:51 +0100

    HW2 - PartC
    Still couldn't find why it's so weird

--- homework2/PartC.m
+++ homework2/PartC.m
@@ -16,7 +16,7 @@ ysd_val = ysd(44:end);
 errorc_train = zeros(30,1);
 errorc_val = zeros(30,1);
 plot([yc_train;yc_val]); hold on;
-for p = 2:2
+for p = 10:10
 [errorc_val(p), errorc_train(p)] = error(yc,yc_train, yc_val, p);
 end
 figure
@@ -31,7 +31,7 @@ plot(1:30, errorc_val);
 errorsd_train = zeros(30,1);
 errorsd_val = zeros(30,1);
 plot([ysd_train;ysd_val]); hold on;
-for p = 2:2
+for p = 10:10
 [errorsd_val(p), errorsd_train(p)] = error(ysd,ysd_train, ysd_val, p);
 end
 figure
@@ -45,36 +45,43 @@ plot(1:30, errorsd_val);
 %------------------------------------------
 function [error_val, error_train] = error(y,y_train, y_val, p)
 N = length(y_train)-1;
+
+% Solve least squares prob
 [alpha, y_train_p] = solveLeastSquares(y_train, p,N);
+
+%Training Error
 error_train = norm(y_train-y_train_p);
+
+%Validation Error
 ytemp = y(N+2-p:end);
 for t = p+1 : length(ytemp)
    ytemp(t) = alpha(1);
    for i = 2:p+1
       ytemp(t) = ytemp(t)+alpha(i)*ytemp(t-i+1);
    end   
-   test = ytemp(t)
-   test1 = ytemp(t-i+1)
 end
 y_val_p = ytemp(p+1:end);
 error_val = norm(y_val-y_val_p);
+
+
 plot([y_train_p;y_val_p], 'DisplayName',strcat('p=', num2str(p))); hold on;
-y_val_p(1)
 end
 
 function [alpha, y_p] = solveLeastSquares(y,p,N)
 A = zeros(N+1,p+1);
 A(:,1) = 1;
 for j = 1:p
-    A(j+1:p,j+1) = y(1:p-j);
-    A(p+1:end,p+2-j) = y(j:j+(N-p));
+    A(j+1:end,j+1) = y(1:end-j);
 end
 b=y;
 
+%QR decomp
 [Q,R] = qr(A);
 Qprim = Q(:,1:p+1);
 Rprim = R(1:p+1,:);
+%TODO : backward substitution
 alpha = Rprim\(Qprim'*b);
 
+%model
 y_p = A*alpha;
 end
